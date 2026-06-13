b0639ac65724dd069a465f18a614dd5bc31300ee
kolivn <kolivn@pseraya.com.sg>
2026-06-13 11:09:57 +0530

    Fix crash on launch: move gesture-handler to index.ts, remove reanimated babel plugin

--- App.tsx
+++ App.tsx
@@ -1,4 +1,3 @@
-import 'react-native-gesture-handler';
 import React, { useEffect } from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { GestureHandlerRootView } from 'react-native-gesture-handler';
